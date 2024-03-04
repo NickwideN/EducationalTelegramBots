24ca21a637bed9ea2b75247e15526d44adf1cdb1
NickwideN <deynega.nv@phystech.edu>
2024-03-04 14:17:00 +0400

    Make test bot send inline keyboards

--- bots/tests/main.py
+++ bots/tests/main.py
@@ -1,6 +1,6 @@
 from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
-from aiogram.types import Message, KeyboardButton, ReplyKeyboardMarkup
+from aiogram.types import Message, KeyboardButton, ReplyKeyboardMarkup, InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
 from config_reader import env
@@ -13,21 +13,35 @@ dp = Dispatcher()
 # Этот хэндлер будет срабатывать на команду "/start"
 @dp.message(Command(commands=["start"]))
 async def process_start_command(message: Message):
-    # Создаем список списков с кнопками
-    keyboard: list[list[KeyboardButton]] = [
-        [KeyboardButton(text=str(i)) for i in range(1, 4)],
-        [KeyboardButton(text=str(i)) for i in range(4, 7)]
-    ]
 
-    keyboard.append([KeyboardButton(text='7')])
-
-    # Создаем объект клавиатуры, добавляя в него кнопки
-    my_keyboard = ReplyKeyboardMarkup(
-        keyboard=keyboard,
-        resize_keyboard=True
+    url_button_1: InlineKeyboardButton = InlineKeyboardButton(
+        text="Мой телеграм канал",
+        url="tg://resolve?domain=nickwiden_life"
     )
 
-    await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь', reply_markup=my_keyboard)
+    url_button_2: InlineKeyboardButton = InlineKeyboardButton(
+        text="Где то тут я учусь",
+        url="https://stepik.org/12092"
+    )
+
+    url_button_3: InlineKeyboardButton = InlineKeyboardButton(
+        text="Мой телеграм канал еще раз",
+        url="tg://resolve?domain=nickwiden_life"
+    )
+
+    # Создаем список списков с кнопками
+    keyboard: list[list[InlineKeyboardButton]] = [
+        [url_button_1, url_button_2],
+        [url_button_3]
+    ]
+
+    # Создаем объект клавиатуры, добавляя в него кнопки
+    my_keyboard = InlineKeyboardMarkup(
+        inline_keyboard=keyboard,
+    )
+
+    await message.answer('Привет!\nБот для тестов на связи!\n'
+                         'С моей помощью мой хозяин упражняется в реализации разных фич', reply_markup=my_keyboard)
 
 
 # Этот хэндлер будет срабатывать на команду "/help"
